ba4001aae8cf515a8c45eb92c24d347e168aeed8
??? <marvin.sincini@studenti.unicam.it>
2026-04-09 02:12:13 +0200

    inserita sequenza di comandi su main

--- app/src/main/java/mdp2026/pokedex/App.java
+++ app/src/main/java/mdp2026/pokedex/App.java
@@ -1,7 +1,20 @@
 package mdp2026.pokedex;
 
+import mdp2026.pokedex.console.BasicPokedexConsoleView;
+import mdp2026.pokedex.console.IOConsoleView;
+import mdp2026.pokedex.model.JsonFilePokedexGetter;
+import mdp2026.pokedex.model.Pokedex;
+import mdp2026.pokedex.model.PokedexGetter;
+import mdp2026.pokedex.utility.ScannerTextInput;
+import mdp2026.pokedex.utility.StandardTextOutput;
+
 public class App {
     public static void main(String[] args) {
-        
+        PokedexGetter builder = new JsonFilePokedexGetter();
+        Pokedex pokedex = builder.buildPokedex();
+        IOConsoleView view = new BasicPokedexConsoleView(pokedex);
+        view.setInput(new ScannerTextInput())
+            .setOutput(new StandardTextOutput())
+            .start();
     }
 }
